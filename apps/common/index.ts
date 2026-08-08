a5429ff38fea9d414f15783c486c42e0323d11f2
hangtiancheng.htc <hangtiancheng.htc@taobao.com>
2026-08-08 13:40:02 +0800

    chore: remove unused @fe26/common workspace package
    Neither apps/server nor apps/client imports it; contracts are kept as
    local zod schemas in each app.

--- apps/common/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./src/chat.js";
-export * from "./src/course.js";
-export * from "./src/learn.js";
-export * from "./src/tracker.js";
-export * from "./src/user.js";
-export * from "./src/word.js";
--- apps/common/src/learn.ts
+++
@@ -1,3 +0,0 @@
-export interface ResultLearn {
-  wordNumber: number;
-}
